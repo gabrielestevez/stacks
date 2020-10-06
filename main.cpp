5e5865d81170c720302147c1069b1567f1236d1b
Gabriel Estevez Cabello <g.esteves@usp.ceu.es>
2020-10-06 22:48:14 +0200

    Ejercicio hecho pero con fechas como string

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 using namespace std;
 struct vox{
-    int v;
+    string nombre;
+    int exp;
+    string fecha;
     vox *sgt;
 };
 vox *peek = NULL;
-void push(int x){
-   vox *nvox =  new vox;
-   nvox->v=x,nvox->sgt=peek;
-   peek=nvox;
+void push(string x, int y, string z){
+    vox *nvox = new vox;
+    nvox->nombre=x,nvox->sgt=peek;
+    nvox->exp=y,nvox->sgt=peek;
+    nvox->fecha,nvox->sgt=peek;
+    peek=nvox;
+    //nvox->nombre="Gabriel";
 }
 void pop(){
     if(peek==NULL)
@@ -22,19 +27,22 @@ void prt(){
          cout<<"Nada que imprimir, Stack vacia\n";
      else
          while(aux!=NULL){
-             cout<<aux->v<<" -> ";
+             cout<<aux->nombre<<" -> ";
              aux=aux->sgt;
          }
 }
 int main(){
-    int op, x;
+    int op;
+    string x;
+    int y;
+    string z;
     do{
         cout<<"\n(1) Push    (2) Pop    (3) Print   (4) Salir \n ";
         cout<<"\n Elija ppcion  ?: ",cin>>op;
         switch (op){
             case 1:
                 cout<<"Ingresa el valor a insertar en el Stack : ??  ",cin>>x;
-                push(x);
+                push(x,y,z);
                 break;
             case 2:
                 cout<<"Eliminando ..... : ";
